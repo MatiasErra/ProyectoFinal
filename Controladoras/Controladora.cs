5ede4019739bf67eaaef3c89bd2a56588bf499d4
MatiasErra <matiaserramouspe@gmail.com>
2023-06-29 19:25:45 -0300

    Documentacion

---
+++ Controladoras/Controladora.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Clases;
+
+namespace Controladoras
+{ 
+	public class ControladoraPersonas
+	{
+		public List<Camionero> ListaCamioneros = new List<Camionero>();
+        public List<Cliente> ListaClientes = new List<Cliente>();
+        public List<Admin> ListaAdmins = new List<Admin>();
+    }
+}
