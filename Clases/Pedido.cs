3c391ac4e334d9627812ae933fede42ac97b49df
MatiasErra <matiaserramouspe@gmail.com>
2023-10-02 15:53:57 -0300

    0.3.12

--- Clases/Pedido.cs
+++ Clases/Pedido.cs
@@ -15,7 +15,7 @@ namespace Clases
 
         private string _fechaPedido;
         private string _fechaEntre;
-        private string _fechaEspe;
+     
       
         private double _costo;
         private string _informacionEnvio;
@@ -24,18 +24,6 @@ namespace Clases
  
 
 
-        public Pedido(int idPedido, int idCliente, string estado, string fechaEntre, string fechaEspe, double costo, string informacionEnvio)
-        {
-            IdPedido = idPedido;
-            IdCliente = idCliente;
-            Estado = estado;
-            FechaEntre = fechaEntre;
-            FechaEspe = fechaEspe;
-  
-            Costo = costo;
-            InformacionEnvio = informacionEnvio;
-          
-        }
 
         public Pedido() { }
 
@@ -43,7 +31,7 @@ namespace Clases
         public int IdCliente { get => _idCliente; set => _idCliente = value; }
         public string Estado { get => _estado; set => _estado = value; }
         public string FechaEntre { get => _fechaEntre; set => _fechaEntre = value; }
-        public string FechaEspe { get => _fechaEspe; set => _fechaEspe = value; }
+
     
         public double Costo { get => _costo; set => _costo = value; }
         public string InformacionEnvio { get => _informacionEnvio; set => _informacionEnvio = value; }
